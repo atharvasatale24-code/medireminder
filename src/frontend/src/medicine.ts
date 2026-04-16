faa763f08bad9fc94fb2d3ab372593186cdc8777
atharvasatale24-code <atharva.satale24@mmit.edu.in>
2026-04-16 23:00:45 +0530

    Create medicine.ts

---
+++ src/frontend/src/medicine.ts
@@ -0,0 +1,21 @@
+import { collection, getDocs } from "firebase/firestore";
+import db from "./db";
+
+export async function getMedicines() {
+  const snapshot = await getDocs(collection(db, "medicines"));
+  return snapshot.docs.map(doc => doc.data());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
